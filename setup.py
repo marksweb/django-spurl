409fd6af4e526d3767c76b9c63a5fa2c0536d2af
Mark Walker <theshow@gmail.com>
2018-01-30 16:01:49 +0000

    Specifying less than django 2.0
    Running `pip install -r requirements.txt` with `django-spurl==0.6.4` in it is currently trying to install django 2 for Spurl on a new server.

--- setup.py
+++ setup.py
@@ -11,17 +11,9 @@ def read(*parts):
     file_path = os.path.join(os.path.dirname(__file__), *parts)
     return codecs.open(file_path, encoding='utf-8').read()
 
-
-def find_version(*parts):
-    version_file = read(*parts)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return str(version_match.group(1))
-    raise RuntimeError("Unable to find version string.")
-
 setup(
     name='django-spurl',
-    version=find_version('spurl', '__init__.py'),
+    version='0.6.4',
     license='Public Domain',
 
     install_requires=[
@@ -30,8 +22,7 @@ setup(
     ],
     setup_requires=[
         'urlobject>=2.4.0',
-        'django>=1.4',
-        'nose',
+        'django>=1.4,<2.0',
         'six',
     ],
 
@@ -50,23 +41,5 @@ setup(
     packages=find_packages(exclude=('example*', '*.tests*')),
     include_package_data=True,
 
-    test_suite='nose.collector',
-    tests_require=['nose'],
-
     zip_safe=False,
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: Public Domain',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
 )
